7140169024b15af5945d98798d1d0d6a0c41c848
hariPrasad0647 <hariprasad0647@gmail.com>
2025-06-08 18:12:49 +0530

    added build folder

--- games_admin/src/App.js
+++ games_admin/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminLogin from './Components/adminLogin'
 import HomePage from './Components/homePage';
 import './App.css';
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route exact path = "/login" Component={AdminLogin} />
           <Route exact path ="/" Component={HomePage} />
         </Routes>
     </BrowserRouter>
