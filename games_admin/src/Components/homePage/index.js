0ea941acf71e8f3b49c0ea9ac3b4e72981e1d8af
hariPrasad0647 <hariprasad0647@gmail.com>
2025-06-09 12:51:52 +0530

    added env file to access deployed backend url

--- games_admin/src/Components/homePage/index.js
+++ games_admin/src/Components/homePage/index.js
@@ -37,7 +37,7 @@ const HomePage = () => {
                 screenshots: [formValues.ss1, formValues.ss2, formValues.ss3, formValues.ss4]
             }
 
-            fetch(`http://localhost:5000/api/${collection}`, {
+            fetch(`${process.env.REACT_APP_API_URL}/api/${collection}`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(finalData)
